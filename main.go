9e581bacad93253a0a530a6937df99361e546de2
ponte1010 <t.miwa.528@nitech.jp>
2022-07-28 18:08:14 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"protobuf-lesson/pb"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	employee := &pb.Employee{
+		Id:          1,
+		Name:        "Suzuki",
+		Email:       "test@example.com",
+		Occupation:  pb.Occupation_ENGINEER,
+		PhoneNumber: []string{"080-1234-5678", "090-1234-5678"},
+		Project:     map[string]*pb.Company_Project{"ProjectX": &pb.Company_Project{}},
+		Profile: &pb.Employee_Text{
+			Text: "My name is Suzuki",
+		},
+		Birthday: &pb.Date{
+			Year:  2000,
+			Month: 1,
+			Day:   1,
+		},
+	}
+
+	binData, err := proto.Marshal(employee)
+	if err != nil {
+		log.Fatalln("Can't serialize", err)
+	}
+
+	if err := ioutil.WriteFile("test.bin", binData, 0666); err != nil {
+		log.Fatalln("Can't write", err)
+	}
+
+	in, err := ioutil.ReadFile("test.bin")
+	if err != nil {
+		log.Fatalln("Can't read file", err)
+	}
+
+	readEmployee := &pb.Employee{}
+
+	err = proto.Unmarshal(in, readEmployee)
+	if err != nil {
+		log.Fatalln("Can't deserialize", err)
+	}
+
+	fmt.Println(readEmployee)
+}
